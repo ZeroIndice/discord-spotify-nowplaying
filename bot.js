de47cb52df60babdbf6af02880c7bac9f9ee8213
Jagger McCord <Jagger McCord>
2016-01-13 15:43:03 -0600

    stop showing current song when paused.

--- bot.js
+++ bot.js
@@ -12,40 +12,47 @@ var bot = new DiscordClient({
 
 bot.on('ready', function () {
     console.log(bot.username + " - (" + bot.id + ")");
-    bot.on('disconnected', function(){
-        setTimeout(function(){
-        bot.connect();
+    bot.on('disconnected', function () {
+        setTimeout(function () {
+            bot.connect();
         }, 5000);
     });
 
 });
- 
- var curr = "";
+
+var curr = "";
 // get the name of the song which is currently playing 
-setInterval(function(){spotify.getStatus(function (err, res) {
-  if (err) {
-    return console.error(err);
-  }
-  
-  var next = "♪" + res.track.track_resource.name + " - " + res.track.artist_resource.name + "♪";
-  if(next == curr) return;
-  console.log(next);
-	bot.setPresence({
-        game: next
-    });
-	curr = next;
-});
-    }, 2000);
+setInterval(function () {
+    spotify.getStatus(function (err, res) {
+        if (err) {
+            return console.error(err);
+        }
+        if (res.playing) {
+            var next = "♪" + res.track.track_resource.name + " - " + res.track.artist_resource.name + "♪";
+            if (next == curr) return;
+            //console.log(next);
+            bot.setPresence({
+                game: next
+            });
+            curr = next;
+        } else {
+            curr = "";
+            bot.setPresence({
+                game: ""
+            });
+        }
+    })
+}, 3000);
 
 bot.on('message', function (_user, userID, channelID, message, rawEvent) {
-    if(message == (bot.username+ ", music link?")){
+    if (message == (bot.username + ", music link?")) {
         spotify.getStatus(function (err, res) {
             if (err) {
-               return console.error(err);
+                return console.error(err);
             }
             bot.sendMessage({
-                to:channelID,
-                message:"http://open.spotify.com/" + res.track.track_resource.uri.split(":").slice(1).join("/")
+                to: channelID,
+                message: "http://open.spotify.com/" + res.track.track_resource.uri.split(":").slice(1).join("/")
             })
         });
     }
