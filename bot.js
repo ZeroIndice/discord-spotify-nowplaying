e4018005ce4584c8e9d73b5272a150a6cfb7dba4
Jagger McCord <Jagger McCord>
2016-01-13 15:20:41 -0600

    intial commit

---
+++ bot.js
@@ -0,0 +1,52 @@
+var DiscordClient = require('discord.io');
+var nodeSpotifyWebHelper = require('node-spotify-webhelper');
+
+var spotify = new nodeSpotifyWebHelper.SpotifyWebHelper();
+var config = require("./config");
+var bot = new DiscordClient({
+    autorun: true,
+    email: config.email,
+    password: config.password,
+    username: config.username
+});
+
+bot.on('ready', function () {
+    console.log(bot.username + " - (" + bot.id + ")");
+    bot.on('disconnected', function(){
+        setTimeout(function(){
+        bot.connect();
+        }, 5000);
+    });
+
+});
+ 
+ var curr = "";
+// get the name of the song which is currently playing 
+setInterval(function(){spotify.getStatus(function (err, res) {
+  if (err) {
+    return console.error(err);
+  }
+  
+  var next = "♪" + res.track.track_resource.name + " - " + res.track.artist_resource.name + "♪";
+  if(next == curr) return;
+  console.log(next);
+	bot.setPresence({
+        game: next
+    });
+	curr = next;
+});
+    }, 2000);
+
+bot.on('message', function (_user, userID, channelID, message, rawEvent) {
+    if(message == (bot.username+ ", music link?")){
+        spotify.getStatus(function (err, res) {
+            if (err) {
+               return console.error(err);
+            }
+            bot.sendMessage({
+                to:channelID,
+                message:"http://open.spotify.com/" + res.track.track_resource.uri.split(":").slice(1).join("/")
+            })
+        });
+    }
+});
